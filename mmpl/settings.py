fdd50373e7b298b3da33a1513f0e7f2ea298e9c5
Ben Elliot <benjiboi214@gmail.com>
2016-02-12 23:28:53 +1100

    Broke what I had written in the admin anhd models files in the existing app.
    Have moved to calling the base app 'core' and will rebuild the model and admin page based on the obvious requirements of the newly staged templates folder.
    
    Templates folder contains the beginnings of the base website. Need to djangofy it by adding template tags and stuff. Make sure the html has the static links in it.

--- mmpl/settings.py
+++ mmpl/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'mmpl.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -88,7 +89,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Australia/Melbourne'
 
 USE_I18N = True
 
---
+++ templates/js/mmpl-jquery.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    
+    $('#hide-me').click(function(){
+        $('#jumbotron').slideUp();
+    });
+    
+});

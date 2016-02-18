ad0f3838ade403e4b79765b8e25ba155656db07a
Ben Elliot <benjiboi214@gmail.com>
2016-02-18 23:40:44 +1100

    Post Model and Admin page configured and working. -Made sure author is written when post is saved -Author not editable after post saved. -Date is written as datetime object when model saved -Date is also not editable once posted. -Admin is searchable.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from core.models import Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ['author', 'date',]
+    #exclude = ['author', 'date',]
+    fieldsets = [
+        (None, {'fields': ['title', 'body']}),
+        ('Details', {'fields': ['category', 'author', 'date', 'feature'], 
+                     'classes': ['collapse']}),
+    ]
+    list_display = ('title', 'date', 'feature',)
+    list_filter = ('date',)
+    search_fields = ['title', 'body',]
+    
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.author = User.objects.get(id = request.user.id)
+        obj.save()
+
+
+admin.site.register(Post, PostAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    CATEGORY_CHOICES = (
+        ('NE', 'News'),
+        ('EV', 'Events'),
+        ('RU', 'Results'),
+        ('RO', 'Resources'),
+    )
+    author = models.ForeignKey(User, verbose_name='Author')
+    date = models.DateTimeField(auto_now_add=True, verbose_name='Published')
+    category = models.CharField(max_length=2,
+    							choices=CATEGORY_CHOICES,
+    							default='NE',
+    							verbose_name='Category')
+    title = models.CharField(max_length=128, verbose_name='Title')
+    body = models.TextField(verbose_name='Content')
+    feature = models.BooleanField(default=True, verbose_name='Featured')
+    #image = 
+    #files = 
+        
+    def __unicode__(self):
+        return self.title

2e93c4393a7f117a11e98d27a5c6beb0d7afefa3
Ben Elliot <benjiboi214@gmail.com>
2016-02-19 23:14:54 +1100

    Got first view up and running. -Moved the templates to the right static dir. -Renamed base.html references to use django static stuff. -Created feed.html to inherit from base and create staging area for feed.

--- core/views.py
+++ core/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from core.models import Post
 
 def index(request):
-    return HttpResponse("MMPL Test!")
+    posts = Post.objects.order_by()[:5]
+    
+    context_dict = {'posts': posts}
+    
+    response = render(request, 'core/feed.html', context_dict)
+    
+    return response
 
 # Create your views here.

5f54071526c4f28c14b7eca008664cd6f0dfa86a
Ben Elliot <benjiboi214@gmail.com>
2016-06-07 20:24:00 +1000

    Changing settings.py again for deployment

--- mmpl/settings.py
+++ mmpl/settings.py
@@ -102,10 +102,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Path to the media files hosted on the OS
 MEDIA_URL = '/media/'  # URL for the media files to be svered from.

fbaabd9eb5e41fbd7b4d4856d7014fb5ccd63c08
Ben Elliot <benjiboi214@gmail.com>
2016-06-07 21:22:06 +1000

    FUck me

--- mmpl/settings.py
+++ mmpl/settings.py
@@ -102,8 +102,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-if not DEBUG:
-    STATIC_ROOT = '/opt/mmpl/static/'
+STATIC_ROOT = '/opt/mmpl/static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
